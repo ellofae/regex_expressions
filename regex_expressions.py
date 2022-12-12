54b3ea729464185a8e3d1f8135a354ea0c030890
ellofae <sergei.bykovskiy2003@gmail.com>
2022-12-13 00:03:49 +0300

    py file modified

--- regex_expressions.py
+++ regex_expressions.py
@@ -4,12 +4,6 @@
 
 import re
 
-emails = '''
-CoreyMSchafer@gmail.com
-corey.schafer@university.edu
-corey-321-schafer@my-work.net
-'''
-
 #pattern = re.compile(r'[a-zA-Z0-9.-_]+@[a-zA-Z-]+\.(com|edu|net|ru|du)')
 pattern = re.compile(r'[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
 
